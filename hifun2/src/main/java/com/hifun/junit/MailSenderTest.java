4bd4e31d627a5c07a73ab8f789fea8c5680f6887
powlin <linyaoyan@qq.com>
2016-03-26 11:53:27 +0800

    hf

--- hifun2/src/main/java/com/hifun/junit/MailSenderTest.java
+++ hifun2/src/main/java/com/hifun/junit/MailSenderTest.java
@@ -7,25 +7,25 @@ import com.hifun.util.mail.SimpleMailSender;
 
 public class MailSenderTest {
 
-	@Test
-	public void sendMail(){
-		String[] tos;
-		String[] ccs;
-		MailSenderInfo mailInfo = new MailSenderInfo();
-		mailInfo.setMailServerPort("25");
-		mailInfo.setUserName("1321967761@qq.com");
-		mailInfo.setPassword("linyy18258155523");
-		mailInfo.setFromAddress("1321967761@qq.com");
-		mailInfo.setValidate(true);
-		mailInfo.setSubject("my subject");
-		tos = new String[]{"1321967761@qq.com"};
-		ccs = new String[]{"1321967761@qq.com"};
-		mailInfo.setContent("<table><tr><td>123</td></tr></table>");
-		mailInfo.setTos(tos);
-		mailInfo.setCcs(ccs);
-//		SendMailThread sendMailThread = new SendMailThread(mailInfo);
-//		new Thread(sendMailThread).start();
-		SimpleMailSender.sendMail(mailInfo);
-	}
-	
+    @Test
+    public void sendMail() {
+        String[] tos;
+        String[] ccs;
+        MailSenderInfo mailInfo = new MailSenderInfo();
+        mailInfo.setMailServerPort("25");
+        mailInfo.setUserName("1321967761@qq.com");
+        mailInfo.setPassword("");
+        mailInfo.setFromAddress("1321967761@qq.com");
+        mailInfo.setValidate(true);
+        mailInfo.setSubject("my subject");
+        tos = new String[] { "1321967761@qq.com" };
+        ccs = new String[] { "1321967761@qq.com" };
+        mailInfo.setContent("<table><tr><td>123</td></tr></table>");
+        mailInfo.setTos(tos);
+        mailInfo.setCcs(ccs);
+        // SendMailThread sendMailThread = new SendMailThread(mailInfo);
+        // new Thread(sendMailThread).start();
+        SimpleMailSender.sendMail(mailInfo);
+    }
+
 }
