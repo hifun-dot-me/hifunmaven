046bb7bcf8941dcd27af3f98b50d830f9c4eaa85
powlin <linyaoyan@qq.com>
2016-04-04 17:21:35 +0800

    调整参数过滤规则

--- hifun2/src/main/java/com/hifun/base/EncodeRequestWrapper.java
+++ hifun2/src/main/java/com/hifun/base/EncodeRequestWrapper.java
@@ -7,22 +7,24 @@ import com.hifun.util.EncodeChangeUtil;
 
 public class EncodeRequestWrapper extends HttpServletRequestWrapper {
 
-	//不做过滤的参数名称
-	private static final String VALIDATE_PARAMETER = "password";
-	
+    // 不做过滤的参数名称
+    // 1.密码
+    // 2. ueditor-html文本
+    private static final String VALIDATE_PARAMETERS = "password, content";
+
     public EncodeRequestWrapper(HttpServletRequest request) {
         super(request);
     }
-    
+
     @Override
     public String[] getParameterValues(String name) {
-    	String[] values = super.getParameterValues(name);
-    	if(!VALIDATE_PARAMETER.equals(name) && values != null){
-    		for (int i = 0; i < values.length; i++) {
+        String[] values = super.getParameterValues(name);
+        if (!VALIDATE_PARAMETERS.contains(name) && values != null) {
+            for (int i = 0; i < values.length; i++) {
                 values[i] = EncodeChangeUtil.change(values[i]);
             }
-    	}
-    	return values;
+        }
+        return values;
     }
 
 }
